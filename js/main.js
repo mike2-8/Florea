ec5f54846ba79bd6d45c3b7f594057f37c9c573d
Mike-2808 <chmaicntt2211002@student.ctuet.edu.vn>
2026-06-17 17:17:42 +0700

    update sections

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
 // 1. Dropdown hiện khi hover chuột vào (không cần click)
 
-document.querySelectorAll('.nav-item.dropdown').forEach(function(dropdown) {
+document.querySelectorAll('.nav-item.dropdown').forEach(function (dropdown) {
     let timeout;
 
-    dropdown.addEventListener('mouseenter', function() {
+    dropdown.addEventListener('mouseenter', function () {
         clearTimeout(timeout);
         const menu = this.querySelector('.dropdown-menu');
         menu.classList.add('show');
     });
 
-    dropdown.addEventListener('mouseleave', function() {
+    dropdown.addEventListener('mouseleave', function () {
         const menu = this.querySelector('.dropdown-menu');
-        timeout = setTimeout(function() {
+        timeout = setTimeout(function () {
             menu.classList.remove('show');
         }, 100);
     });
+});
+
+// 2. Hamburger toggle → đổi icon thành X khi mở
+const toggler = document.querySelector('.navbar-toggler');
+const navCollapse = document.querySelector('#navbarMain');
+
+navCollapse.addEventListener('show.bs.collapse', function () {
+    toggler.innerHTML = '<span style="font-size: 24px; color: #E6BBAB;">✕</span>';
+});
+
+navCollapse.addEventListener('hide.bs.collapse', function () {
+    toggler.innerHTML = '<span class="navbar-toggler-icon"></span>';
+});
+
+// 3. testimonial
+var swiper = new Swiper(".testimonial-swiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    //autoplay: {
+    //    delay: 2500,
+    //    disableOnInteraction: false,
+    //},
 });
